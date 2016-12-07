d6b58c91010f8242c0f8a3e30bf09e1aae305a9f
androidexample <example@example.pl>
2016-12-08 00:27:47 +0100

    introduce toolbar and basic ui controls

--- app/src/main/java/pl/edu/agh/androidguiexample/MainActivity.java
+++ app/src/main/java/pl/edu/agh/androidguiexample/MainActivity.java
@@ -2,12 +2,61 @@ package pl.edu.agh.androidguiexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText editText;
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.feed:
+                editText.setText(item.getTitle());
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolBar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolBar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        final CheckBox checkBoxBtn = (CheckBox) findViewById(R.id.checkbox_ok);
+        editText = (EditText) findViewById(R.id.edittext_ok);
+        Button button = (Button) findViewById(R.id.button_ok);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(checkBoxBtn.isChecked()) {
+                    editText.setText(editText.getText().toString().toUpperCase());
+                } else {
+                    editText.setText(editText.getText().toString().toLowerCase());
+                }
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public void clearTextField(MenuItem item) {
+        editText.setText(item.getTitle());
     }
 }
